83ea38471bd13efcedfc3cdda8e9f6d06d779133
Mohit Khapekar <mohitkhapekar@Mohits-MacBook-Air-2.local>
2026-01-28 22:38:23 +0530

    Posters Added

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
 
-#recommended movies display function
+
+def fetch_poster(movie_id):
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=3dd2ebdb7ea7a167988b218876b31aa5&language=en-US'
+    response = requests.get(url)
+    data = response.json()
+    # Add 'w500' or 'original' to the path to get the actual image
+    return "https://image.tmdb.org/t/p/w500" + data['poster_path']
+
+
 def recommend(movie):
-    movie_index = movies[movies['title']==movie].index[0]
+    movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
+    recommended_movies_posters = []
     for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-    return recommended_movies
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
 
 
+# Load data
 movie_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movie_dict)
-
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 st.title('Movie Recommendation System')
-selected_movie_name = st.selectbox('Select Movie Recommendation System',movies['title'].values)
 
-if st.button('Recommend'):
-    recommendations = recommend(selected_movie_name)
-    for i in recommendations:
-        st.write(i)
+selected_movie_name = st.selectbox(
+    'Select a movie to get recommendations',
+    movies['title'].values
+)
+
+if st.button('Show Recommendation'):
+    names, posters = recommend(selected_movie_name)
+
+    # Use st.columns instead of st.beta_columns
+    cols = st.columns(5)
+
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
